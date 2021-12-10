a5fab1f5db45b979d5ea0fb2add495ece6448848
agustinbevi <agustinbevilacqua92@gmail.com>
2021-12-10 20:43:19 -0300

    Test consulta isActiv

--- src/test/java/ProbandoTest.java
+++ src/test/java/ProbandoTest.java
@@ -11,10 +11,19 @@ public class ProbandoTest {
 
 	@Test
 	public void testLlamadaAttraction() {
-		AttractionDAOImpl attraction = new AttractionDAOImpl();
-		Attraction attraction1 = attraction.find(1);
-		
-		System.out.println(attraction1.toString());
+//		AttractionDAOImpl attraction = new AttractionDAOImpl();
+//		Attraction attraction1 = attraction.find(1);
+//		
+//		System.out.println(attraction1.toString());
 	}
 
+//	@Test
+//	public void testAttractions() {
+//		AttractionDAOImpl attraction = new AttractionDAOImpl();
+//		List<Attraction> theAttraction = attraction.findAll();
+//		
+//		System.out.println("");
+//		System.out.println(theAttraction.toString());
+//	}
+	
 }
